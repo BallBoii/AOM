582a912473ae747d1a3ffd6ce87b5f94f5f94c80
ssorawis <ssorawis@gmail.com>
2023-06-28 16:46:50 +0700

    Update flim_main.py
    Add Interactive Gating Function

--- gui/flim_main.py
+++ gui/flim_main.py
@@ -64,6 +64,8 @@ class MainFLIM(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
         print(data['intensity'].shape)
         print(data['tau'].shape)
 
+        self.data = data
+
         nx = int(np.sqrt(data['TCSPC'].shape[0]))  # todo: make this general for non-squared images
 
         img_intensity = np.reshape(data['intensity'], (nx, nx))
@@ -76,41 +78,41 @@ class MainFLIM(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
         self.grid_main.setRowMinimumHeight(0, 400)
 
         # Create PlotItem for each graphs
-        pi_intensity = pg.PlotItem(title='Intensity Image')
-        pi_lifetime = pg.PlotItem(title='FLIM Image')
-        pi_gated = pg.PlotItem(title='Gated Image')
-        pi_hist2d = pg.PlotItem(title='Intensity vs Lifetime')
-        pi_hist1d_intensity = pg.PlotItem(title='Intensity Histogram')
-        pi_hist1d_lifetime = pg.PlotItem(title='Lifetime Histogram')
-        pi_timetrace = pg.PlotItem(title='Fluorescence Time Trace')
-        pi_phasor = pg.PlotItem(title='Phasor')
+        self.pi_intensity = pg.PlotItem(title='Intensity Image')
+        self.pi_lifetime = pg.PlotItem(title='FLIM Image')
+        self.pi_gated = pg.PlotItem(title='Gated Image')
+        self.pi_hist2d = pg.PlotItem(title='Intensity vs Lifetime')
+        self.pi_hist1d_intensity = pg.PlotItem(title='Intensity Histogram')
+        self.pi_hist1d_lifetime = pg.PlotItem(title='Lifetime Histogram')
+        self.pi_timetrace = pg.PlotItem(title='Fluorescence Time Trace')
+        self.pi_phasor = pg.PlotItem(title='Phasor')
 
         # Create ImageView for the images
-        imv_intensity = ImageView(view=pi_intensity)
-        imv_lifetime = ImageView(view=pi_lifetime)
-        imv_gated = ImageView(view=pi_gated)
-        imv_hist2d = ImageView(view=pi_hist2d)
+        self.imv_intensity = ImageView(view=self.pi_intensity)
+        self.imv_lifetime = ImageView(view=self.pi_lifetime)
+        self.imv_gated = ImageView(view=self.pi_gated)
+        self.imv_hist2d = ImageView(view=self.pi_hist2d)
 
-        imv_intensity.setColorMap(cmap_yellowhot)
-        imv_lifetime.setColorMap(cmap_yellowhot)
-        imv_gated.setColorMap(cmap_yellowhot)
-        imv_hist2d.setColorMap(cmap_yellowhot)
+        self.imv_intensity.setColorMap(cmap_yellowhot)
+        self.imv_lifetime.setColorMap(cmap_yellowhot)
+        self.imv_gated.setColorMap(cmap_yellowhot)
+        self.imv_hist2d.setColorMap(cmap_yellowhot)
 
         # Create PlotWidgets for graphs
-        pw_hist1d_intensity = pg.PlotWidget(plotItem=pi_hist1d_intensity)
-        pw_hist1d_lifetime = pg.PlotWidget(plotItem=pi_hist1d_lifetime)
-        pw_timetrace = pg.PlotWidget(plotItem=pi_timetrace)
-        pw_phasor = pg.PlotWidget(plotItem=pi_phasor)
+        self.pw_hist1d_intensity = pg.PlotWidget(plotItem=self.pi_hist1d_intensity)
+        self.pw_hist1d_lifetime = pg.PlotWidget(plotItem=self.pi_hist1d_lifetime)
+        self.pw_timetrace = pg.PlotWidget(plotItem=self.pi_timetrace)
+        self.pw_phasor = pg.PlotWidget(plotItem=self.pi_phasor)
 
         # Add the widgets onto the QGridLayout
-        self.grid_main.addWidget(imv_intensity, 0, 0)
-        self.grid_main.addWidget(imv_lifetime, 0, 1)
-        self.grid_main.addWidget(imv_gated, 0, 2)
-        self.grid_main.addWidget(imv_hist2d, 0, 3)
-        self.grid_main.addWidget(pw_hist1d_intensity, 1, 0)
-        self.grid_main.addWidget(pw_hist1d_lifetime, 1, 1)
-        self.grid_main.addWidget(pw_timetrace, 1, 2)
-        self.grid_main.addWidget(pw_phasor, 1, 3)
+        self.grid_main.addWidget(self.imv_intensity, 0, 0)
+        self.grid_main.addWidget(self.imv_lifetime, 0, 1)
+        self.grid_main.addWidget(self.imv_gated, 0, 2)
+        self.grid_main.addWidget(self.imv_hist2d, 0, 3)
+        self.grid_main.addWidget(self.pw_hist1d_intensity, 1, 0)
+        self.grid_main.addWidget(self.pw_hist1d_lifetime, 1, 1)
+        self.grid_main.addWidget(self.pw_timetrace, 1, 2)
+        self.grid_main.addWidget(self.pw_phasor, 1, 3)
 
         # Process Data
         hist1d_intensity_y, hist1d_intensity_x = np.histogram(img_intensity, bins=100)
@@ -119,28 +121,40 @@ class MainFLIM(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
         img_hist2d, x, y = np.histogram2d(np.squeeze(data['tau']), np.squeeze(data['intensity']), bins=100)
 
         # Display Data
-        imv_intensity.setImage(img_intensity)
-        imv_lifetime.setImage(img_lifetime)
-        imv_gated.setImage(img_intensity) # todo
-        imv_hist2d.setImage(img_hist2d)
+        self.imv_intensity.setImage(img_intensity)
+        self.imv_lifetime.setImage(img_lifetime)
+        self.imv_gated.setImage(img_intensity) # todo
+        self.imv_hist2d.setImage(img_hist2d)
 
         # Plot Graphs
-        pi_hist1d_intensity.plot(hist1d_intensity_x, hist1d_intensity_y, stepMode=True, fillLevel=0, brush=(0, 0, 255, 80))
-        pi_hist1d_lifetime.plot(hist1d_lifetime_x, hist1d_lifetime_y, stepMode=True, fillLevel=0, brush=(0, 0, 255, 80))
-        pi_timetrace.plot(timetrace_y)
+        self.pi_hist1d_intensity.plot(hist1d_intensity_x, hist1d_intensity_y, stepMode=True, fillLevel=0, brush=(0, 0, 255, 80))
+        self.pi_hist1d_lifetime.plot(hist1d_lifetime_x, hist1d_lifetime_y, stepMode=True, fillLevel=0, brush=(0, 0, 255, 80))
+        self.pi_timetrace.plot(timetrace_y)
 
         # Draw Movable Limits for Gating
-        vbar_tmin = pg.InfiniteLine(0, movable=True, angle=90)
-        pi_timetrace.addItem(vbar_tmin)
-        vbar_tmax = pg.InfiniteLine(len(curve_timetrace), movable=True, angle=90)
-        pi_timetrace.addItem(vbar_tmax)
+        self.vbar_tmin = pg.InfiniteLine(0, movable=True, angle=90)
+        self.pi_timetrace.addItem(self.vbar_tmin)
+        self.vbar_tmax = pg.InfiniteLine(len(curve_timetrace), movable=True, angle=90)
+        self.pi_timetrace.addItem(self.vbar_tmax)
+
+        self.vbar_tmin.sigPositionChanged.connect(self.update_gate)
+        self.vbar_tmax.sigPositionChanged.connect(self.update_gate)
 
         # Connect Signals
-        imv_intensity.ui.roiBtn.clicked.connect(imv_lifetime.ui.roiBtn.click)
-        imv_intensity.ui.roiBtn.clicked.connect(imv_gated.ui.roiBtn.click)
+        self.imv_intensity.ui.roiBtn.clicked.connect(self.imv_lifetime.ui.roiBtn.click)
+        self.imv_intensity.ui.roiBtn.clicked.connect(self.imv_gated.ui.roiBtn.click)
 
     def newfunction(self):
         print('new')
+
+    def update_gate(self):
+        imin = round(self.vbar_tmin.getXPos())
+        imax = round(self.vbar_tmax.getXPos())
+
+        nx = int(np.sqrt(self.data['TCSPC'].shape[0]))  # todo: make this general for non-squared images
+
+        img_gated = np.reshape(np.sum(self.data['TCSPC'][:, imin:imax], axis=1), (nx, nx))
+        self.imv_gated.setImage(img_gated)
 
 
 def main():
